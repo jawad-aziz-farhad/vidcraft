6766660ac5125e59b332759b9698965538241aa0
jawad-aziz-farhad <jawad_aziz_farhad@yahoo.com>
2025-01-01 11:58:09 +0500

    constants defined for images and texts.

---
+++ constants/configs.js
@@ -0,0 +1,6 @@
+export const APP_NAME = 'VidCraft';
+export const APP_DESCRIPTION =  'VidCraft is a beautiful app';
+export const HOME_TAB = "Home";
+export const BOOKMARK_TAB = "Mark";
+export const CREATE_TAB = "Create";
+export const PROFILE_TAB = "Profile";
---
+++ constants/icons.js
@@ -0,0 +1,37 @@
+import bookmark from '../assets/icons/bookmark.png';
+import eyeHide from '../assets/icons/eye-hide.png';
+import home from '../assets/icons/home.png';
+import eye from '../assets/icons/eye.png'
+import plus from '../assets/icons/plus.png';
+import profile from '../assets/icons/profile.png';
+import leftArrow from '../assets/icons/left-arrow.png';
+import logout from '../assets/icons/logout.png';
+import menu from '../assets/icons/menu.png';
+import play from '../assets/icons/play.png';
+import rightArrow from '../assets/icons/right-arrow.png';
+import search from '../assets/icons/search.png';
+import upload from '../assets/icons/upload.png';
+import adaptiveIcon from '../assets/adaptive-icon.png';
+import favIcon from '../assets/favicon.png';
+import icon from '../assets/icon.png';
+import splash from '../assets/splash.png';
+
+export default {
+    bookmark,
+    eyeHide,
+    home,
+    eye,
+    plus,
+    profile,
+    leftArrow,
+    logout,
+    menu,
+    play,
+    rightArrow,
+    search,
+    upload,
+    adaptiveIcon,
+    favIcon,
+    icon,
+    splash,
+}
