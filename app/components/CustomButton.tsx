eb41e19884a2bc0ab651fa4a02e66d9bcb25c434
jawad-aziz-farhad <jawad_aziz_farhad@yahoo.com>
2025-01-01 12:08:57 +0500

    CustomButton component created.

---
+++ app/components/CustomButton.tsx
@@ -0,0 +1,36 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { isLoaded, isLoading } from "expo-font";
+
+interface CustomButtonInterface {
+  title: string;
+  handlePress?: () => void;
+  textStyles?: string;
+  containerStyle?: string;
+  isLoading?: boolean;
+}
+
+const CustomButton = ({
+  title,
+  handlePress,
+  containerStyle,
+  textStyles,
+  isLoading,
+}: CustomButtonInterface) => {
+  return (
+    <TouchableOpacity
+      className={`bg-secondary rounded-xl min-h-[62px] 
+        justify-center items-center ${containerStyle}
+        (${isLoading} ? ' opacity-50' : ' opacity-100')`}
+      activeOpacity={0.7}
+      onPress={handlePress}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
